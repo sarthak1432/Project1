5cc6b2966cf6799b7ea2e8142ab33040a4986c03
sarthak1432 <145658092+sarthak1432@users.noreply.github.com>
2026-03-03 12:47:27 +0530

    Secure credentials

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,13 +1,13 @@
-import {initializeApp} from "firebase/app";
-import {getFirestore} from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig ={
-  apiKey: "AIzaSyDIuK2nJMXacF368FbTpbOOhCsEpydcAU4",
-  authDomain: "invoice-generator-d9511.firebaseapp.com",
-  projectId: "invoice-generator-d9511",
-  storageBucket: "invoice-generator-d9511.firebasestorage.app",
-  messagingSenderId: "361157797508",
-  appId: "1:361157797508:web:8806804b2a6e00039f04d1"
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
